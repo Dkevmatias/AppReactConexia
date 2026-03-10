0059bce111d03a941229f735155c7ce3b89ede79
Dkevmatias <kmatias137@gmail.com>
2026-03-10 15:19:21 -0600

    pdf changes

--- src/pages/Dashboard/Terminos.tsx
+++ src/pages/Dashboard/Terminos.tsx
@@ -1,19 +1,14 @@
 const Terminos = () => {
   return (
     <div style={{ height: "100vh", width: "100%" }}>
-      <object
-        data="/legal/TerminosCondiciones.pdf"
-        type="application/pdf"
+      <iframe
+        src="/legal/TerminosCondiciones.pdf"
+        title="Términos y Condiciones"
         width="100%"
         height="100%"
-      >
-        <p>
-          No se puede mostrar el PDF.  
-          <a href="/legal/TerminosCondiciones.pdf" target="_blank" rel="noopener noreferrer">
-            Descargar PDF
-          </a>
-        </p>
-      </object>
+        style={{ border: "none" }}
+        
+      />
     </div>
   );
 };
