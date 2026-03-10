ef9555f5d9fed452bb21051d545bd049a7f735ab
Dkevmatias <kmatias137@gmail.com>
2026-03-10 15:30:00 -0600

    visor de pdf

--- src/pages/Dashboard/Terminos.tsx
+++ src/pages/Dashboard/Terminos.tsx
@@ -1,18 +1,15 @@
+import { Document, Page, pdfjs } from "react-pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc =
+  `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
 const Terminos = () => {
   return (
-    <div style={{ height: "100vh", width: "100%" }}>
-       <object
-        data="/legal/TerminosCondiciones.pdf"
-        type="application/pdf"
-        width="100%"
-        height="100%"
-      >
-        <a href="/legal/TerminosCondiciones.pdf">
-          Descargar PDF
-        </a>
-      </object>
+    <div style={{ height: "100vh", overflow: "auto", textAlign: "center" }}>
+      <Document file="/legal/TerminosCondiciones.pdf">
+        <Page pageNumber={1} width={900} />
+      </Document>
     </div>
   );
 };
-
 export default Terminos;
