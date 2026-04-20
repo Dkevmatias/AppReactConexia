ddcf7e8825c05078c979e9da6ba52a5ab3bf285b
Dkevmatias <kmatias137@gmail.com>
2026-04-20 15:18:32 -0600

    Cambio en Terminos y Condiciones para la parte mobil

--- src/pages/Dashboard/Terminos.tsx
+++ src/pages/Dashboard/Terminos.tsx
@@ -1,15 +1,18 @@
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
 const Terminos = () => {
+  if (isMobile) {
+    window.location.href = "/legal/TerminosCondiciones.pdf";
+    return null;
+  }
+
   return (
-    <div style={{ height: "100vh", width: "100%" }}>
-      <iframe
-        src="/legal/TerminosCondiciones.pdf"
-        title="Términos y Condiciones"
-        width="100%"
-        height="100%"
-        style={{ border: "none" }}
-      />
-    </div>
+    <iframe
+      src="/legal/TerminosCondiciones.pdf"
+      width="100%"
+      height="100%"
+      style={{ border: "none" }}
+    />
   );
 };
-
 export default Terminos;
